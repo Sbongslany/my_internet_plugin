0007b66f4b2bd26547d38d26ab924ee21aa13c9b
Sibongiseni Ndebele <43204005+Sbongslany@users.noreply.github.com>
2024-04-15 17:39:45 +0200

    android native kotlin plugin

--- android/src/main/kotlin/com/example/my_internet_plugin/MyInternetPlugin.kt
+++ android/src/main/kotlin/com/example/my_internet_plugin/MyInternetPlugin.kt
@@ -1,35 +1,85 @@
 package com.example.my_internet_plugin
 
-import androidx.annotation.NonNull
-
-import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
 
-/** MyInternetPlugin */
-class MyInternetPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class MyInternetPlugin private constructor(context: Context, messenger: BinaryMessenger) : MethodChannel.MethodCallHandler {
+  private val context: Context
+  private val connectivityManager: ConnectivityManager
+  private var eventSink: EventChannel.EventSink? = null
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "my_internet_plugin")
-    channel.setMethodCallHandler(this)
+  init {
+    this.context = context
+    connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val eventChannel = EventChannel(messenger, "my_internet_plugin/connectivity")
+    eventChannel.setStreamHandler(object : StreamHandler() {
+      @Override
+      fun onListen(arguments: Object?, sink: EventChannel.EventSink?) {
+        eventSink = sink
+        startListening()
+      }
+
+      @Override
+      fun onCancel(arguments: Object?) {
+        stopListening()
+      }
+    })
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+  @Override
+  fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+    if (call.method.equals("checkInternet")) {
+      val isConnected = checkInternetConnection()
+      result.success(isConnected)
     } else {
       result.notImplemented()
     }
   }
 
-  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+  private fun startListening() {
+    val networkCallback: ConnectivityManager.NetworkCallback = object : NetworkCallback() {
+      @Override
+      fun onAvailable(network: Network?) {
+        super.onAvailable(network)
+        if (eventSink != null) {
+          eventSink.success(true)
+        }
+      }
+
+      @Override
+      fun onLost(network: Network?) {
+        super.onLost(network)
+        if (eventSink != null) {
+          eventSink.success(false)
+        }
+      }
+    }
+    val builder: NetworkRequest.Builder = Builder()
+    connectivityManager.registerNetworkCallback(builder.build(), networkCallback)
+  }
+
+  private fun stopListening() {
+    connectivityManager.unregisterNetworkCallback(NetworkCallback())
+  }
+
+  private fun checkInternetConnection(): Boolean {
+    val capabilities: NetworkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork())
+    return capabilities != null && (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
+  }
+
+  companion object {
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "my_internet_plugin")
+      val instance = MyInternetPlugin(registrar.context(), registrar.messenger())
+      channel.setMethodCallHandler(instance)
+    }
   }
 }

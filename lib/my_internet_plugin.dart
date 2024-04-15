e17ca0b9f5d4577a5d7f9e91176a5d677a8c5833
Sibongiseni Ndebele <43204005+Sbongslany@users.noreply.github.com>
2024-04-15 17:55:34 +0200

    my internet plugin page

--- lib/my_internet_plugin.dart
+++ lib/my_internet_plugin.dart
@@ -1,14 +1,28 @@
-// You have generated a new plugin project without specifying the `--platforms`
-// flag. A plugin project with no platform support was generated. To add a
-// platform, run `flutter create -t plugin --platforms <platforms> .` under the
-// same directory. You can also find a detailed instruction on how to add
-// platforms in the `pubspec.yaml` at
-// https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
+// This class provides methods to interact with the internet connectivity plugin.
 
-import 'my_internet_plugin_platform_interface.dart';
+import 'dart:async'; // Importing async package for Futures and Streams.
+import 'package:flutter/services.dart'; // Importing services for platform channels.
 
 class MyInternetPlugin {
-  Future<String?> getPlatformVersion() {
-    return MyInternetPluginPlatform.instance.getPlatformVersion();
+  // Creating a MethodChannel to communicate with native code for method calls.
+  static const MethodChannel _channel = MethodChannel('my_internet_plugin');
+
+  // Creating an EventChannel to receive events from native code.
+  static const EventChannel _eventChannel =
+  EventChannel('my_internet_plugin/connectivity');
+
+  // A Stream to listen to connectivity changes.
+  static Stream<bool> get onConnectivityChanged {
+    // Using receiveBroadcastStream to listen to events from the native side and map them to bool values.
+    return _eventChannel.receiveBroadcastStream().map((dynamic isConnected) {
+      return isConnected as bool;
+    });
+  }
+
+  // Method to check internet connectivity.
+  static Future<bool> checkInternet() async {
+    // Invoking the native method 'checkInternet' and returning the result as a Future.
+    final bool isConnected = await _channel.invokeMethod('checkInternet');
+    return isConnected;
   }
 }
